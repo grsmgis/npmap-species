653b28d0bd2cf41eb1802bc0e4108e4033c2a30c
npatel79 <npatel79@vols.utk.edu>
2021-11-22 16:38:04 -0500

    clinked gh-pages/atbirecords

--- scripts/searchUtils.js
+++ scripts/searchUtils.js
@@ -23,7 +23,7 @@ function loadResource(url, callback) {
 
 var can_populate = false;
 function prepareSearchTool() {
-  var atbi = './atbirecords/';
+  var atbi = 'https://github.com/grsmgis/npmap-species/tree/gh-pages/atbirecords/';
 
   var auc = loadResource(atbi + 'species_auc.json', function (data) {
     control._aucValues = data;
